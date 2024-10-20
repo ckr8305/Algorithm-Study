a33d146532222d0389cf0ec3788a381ce77e30c2
KTH1007 <rlaxogjs2684@gmail.com>
2024-10-20 19:08:13 +0900

    백준 10798번

---
+++ taeheon/src/P1020/BOJ10798.java
@@ -0,0 +1,39 @@
+package P1020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ10798 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        char[][] arr = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            Arrays.fill(arr[i], '?');
+        }
+
+        for (int i = 0; i < 5; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < s.length(); j++) {
+                arr[i][j] = s.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != '?')
+                    sb.append(arr[j][i]);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
