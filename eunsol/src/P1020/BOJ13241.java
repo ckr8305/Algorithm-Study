6a101ccb53030df328799b55da019f1fb448ebef
Park EunSol <eunsolguni1353@gmail.com>
2024-10-21 16:14:14 +0900

    백준 13241번

---
+++ eunsol/src/P1020/BOJ13241.java
@@ -0,0 +1,48 @@
+package P1020;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ13241 {
+
+	public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            System.out.println(lcm(a,b));
+        }
+
+        public static long lcm(int a, long b){
+            return a*b / gcd(a,b);
+        }
+
+        public static long gcd(long a, long b){
+            if(b==0) return a;
+            return gcd(b, a % b);
+        }
+
+
+    }
+
+}
