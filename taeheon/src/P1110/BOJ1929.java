68555142de038474220a32ec8e3cf0798c052e3b
KTH1007 <rlaxogjs2684@gmail.com>
2024-10-31 10:08:17 +0900

    백준 1929번

---
+++ taeheon/src/P1110/BOJ1929.java
@@ -0,0 +1,45 @@
+package P1110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1929 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        boolean[] isPrime = prime(N);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    private static boolean[] prime(int n) {
+        boolean[] isPrime = new boolean[n + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        return isPrime;
+    }
+}
