64d6823b6482cad9de4baef45a5e491d625b9b72
KTH1007 <rlaxogjs2684@gmail.com>
2024-11-04 16:09:06 +0900

    백준 10807번

---
+++ taeheon/src/P1110/BOJ10807.java
@@ -0,0 +1,37 @@
+package P1110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ10807 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int v = Integer.parseInt(br.readLine());
+
+        Collections.sort(list);
+
+        int count = 0;
+        if (list.contains(v)) {
+            count = list.lastIndexOf(v) - list.indexOf(v) + 1;
+        } else {
+            count = 0;
+        }
+
+        System.out.println(count);
+    }
+}
