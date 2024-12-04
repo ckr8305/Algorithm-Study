c442dc63e09d999ff6d6481edf2b3ee6cd841504
KTH1007 <rlaxogjs2684@gmail.com>
2024-12-04 11:08:21 +0900

    백준 15552번

---
+++ taeheon/src/P1208/BOJ15552.java
@@ -0,0 +1,23 @@
+package P1208;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15552 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
