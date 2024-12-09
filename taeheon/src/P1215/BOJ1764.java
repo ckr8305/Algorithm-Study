0ce37bd680cbf0b6438489e44a9d37babbb1c414
KTH1007 <rlaxogjs2684@gmail.com>
2024-12-09 13:57:35 +0900

    백준 110764번

---
+++ taeheon/src/P1215/BOJ1764.java
@@ -0,0 +1,44 @@
+package P1215;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ1764 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            String s = br.readLine();
+            if (set.contains(s)) {
+                count++;
+                list.add(s);
+            }
+        }
+
+        list.sort(null);
+
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(count);
+        System.out.println(sb);
+    }
+}
