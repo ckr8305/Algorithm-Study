0c8b3d86aea85c996530e1eb94f0f0eefd16f7d5
ckr8305 <ckr8305@naver.com>
2024-12-17 14:55:21 +0900

    백준 10818번

---
+++ kyulim/src/P1222/BOJ10818.java
@@ -0,0 +1,29 @@
+package P1222;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10818 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] str = br.readLine().split(" ");
+        int[] arr = new int[N];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i <N; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        for (int i = 0; i <N; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+            if (arr[i] < min)
+                min = arr[i];
+        }
+        System.out.println(min + " " + max);
+    }
+
+}
