3482c68a7da72a35a3b609178aff2469375f93c4
ckr8305 <ckr8305@naver.com>
2024-12-25 01:04:42 +0900

    백준 5597번

--- kyulim/src/Y2024/P1229/BOJ5597.java
+++ kyulim/src/Y2024/P1229/BOJ5597.java
@@ -11,7 +11,21 @@ public class BOJ5597 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        boolean[] arr = new boolean[30];
+
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(br.readLine());
+            arr[n - 1] = true;
+        }
+
+        for (int i = 0; i < 30; i++) {
+            if (!arr[i]) {
+                bw.write((i + 1) + "\n");
+            }
+        }
+
         br.close();
+        bw.flush();
         bw.close();
     }
 }
