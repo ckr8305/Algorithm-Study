2b8a34128c6b661958e73d2c490ebbd6042f0286
ckr8305 <ckr8305@naver.com>
2024-12-26 22:19:18 +0900

    백준 2485번

--- kyulim/src/Y2024/P1229/BOJ2485.java
+++ kyulim/src/Y2024/P1229/BOJ2485.java
@@ -11,7 +11,30 @@ public class BOJ2485 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int gcd = 0;
+
+        for (int i = 0; i < N - 1; i++) {
+            int a = arr[i + 1] - arr[i];
+            gcd = findGCD(a, gcd);
+        }
+        bw.write((arr[N - 1] - arr[0]) / gcd + 1 - (arr.length) + " ");
+
         br.close();
+        bw.flush();
         bw.close();
+    }
+    static int findGCD(int A, int B){
+        while(B != 0){
+            int R = A%B;
+            A = B;
+            B = R;
+        }
+        return A;
     }
 }
