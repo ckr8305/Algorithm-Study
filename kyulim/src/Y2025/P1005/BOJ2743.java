e231f1fb3a763964003a0ce949d8ad6002ab9200
ckr8305 <ckr8305@naver.com>
2024-12-29 12:13:35 +0900

    백준 2743번

---
+++ kyulim/src/Y2025/P1005/BOJ2743.java
@@ -0,0 +1,22 @@
+package Y2025.P1005;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ2743 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        bw.write(String.valueOf(str.length()));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
