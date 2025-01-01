a4febfacd954210a66edf0d18ee8b2687fd74239
ckr8305 <ckr8305@naver.com>
2025-01-01 21:34:53 +0900

    패키지 세팅

---
+++ kyulim/src/Y2025/P1005/BOJ17103.java
@@ -0,0 +1,19 @@
+package Y2025.P1005;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ17103 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
