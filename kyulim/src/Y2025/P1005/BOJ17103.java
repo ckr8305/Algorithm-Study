1edc3128e7a6f0a4f3cd2cff183e76f10e09c1e6
ckr8305 <ckr8305@naver.com>
2025-01-03 14:03:49 +0900

    백준 17103번

--- kyulim/src/Y2025/P1005/BOJ17103.java
+++ kyulim/src/Y2025/P1005/BOJ17103.java
@@ -7,13 +7,46 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class BOJ17103 {
+    static boolean[] primeArray = new boolean[1000001];
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        findPrime();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            bw.write(countPartitions(N) + "\n");
+        }
 
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    static void findPrime() {
+        primeArray[0] = primeArray[1] = true;
+
+        for (int i = 2; i * i < primeArray.length; i++) {
+            if (!primeArray[i]) {
+                for (int j = i * i; j < primeArray.length; j += i) {
+                    primeArray[j] = true;
+                }
+            }
+        }
+    }
+
+    static int countPartitions(int N) {
+        if (N % 2 != 0 || N < 4) return 0;
+
+        int count = 0;
+        for (int j = 2; j <= N / 2; j++) {
+            if (!primeArray[j] && !primeArray[N - j]) {
+                count++;
+            }
+        }
+        return count;
     }
 }
