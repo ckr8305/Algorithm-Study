799d0f8641be594fc89e23b7dc23547ffa6a4ab7
KTH1007 <rlaxogjs2684@gmail.com>
2025-01-07 15:14:52 +0900

    백준 11654번

---
+++ taeheon/src/Y2025/P1012/BOJ11654.java
@@ -0,0 +1,14 @@
+package Y2025.P1012;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11654 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println((int) s.charAt(0));
+    }
+}
