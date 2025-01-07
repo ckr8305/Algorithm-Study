9f1e09246e927dd4350253bfd21e4475167ab22f
KTH1007 <rlaxogjs2684@gmail.com>
2025-01-07 15:24:42 +0900

    백준 2675번

---
+++ taeheon/src/Y2025/P1012/BOJ2675.java
@@ -0,0 +1,29 @@
+package Y2025.P1012;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2675 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            String s = st.nextToken();
+
+            for (int i = 0; i < s.length(); i++) {
+                sb.append((s.charAt(i) + "").repeat(r));
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
