50deeda7f5ce1af7b2485863f4715021aaf0fbb3
ckr8305 <ckr8305@naver.com>
2025-01-08 14:34:19 +0900

    백준 11654번

--- kyulim/src/Y2025/P0112/BOJ11654.java
+++ kyulim/src/Y2025/P0112/BOJ11654.java
@@ -1,19 +1,17 @@
 package Y2025.P0112;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class BOJ11654 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char str = br.readLine().charAt(0);
+        System.out.println((int)str);
 
         br.close();
-        bw.flush();
-        bw.close();
     }
 }
