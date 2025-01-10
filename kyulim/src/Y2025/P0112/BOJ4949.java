1af0541586c57b729e01e6e0f85f4ed9003d8f3b
ckr8305 <ckr8305@naver.com>
2025-01-10 12:04:15 +0900

    백준 4949번

--- kyulim/src/Y2025/P0112/BOJ4949.java
+++ kyulim/src/Y2025/P0112/BOJ4949.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class BOJ4949 {
     public static void main(String[] args) throws IOException {
@@ -12,8 +13,38 @@ public class BOJ4949 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        while (true) {
+            String str = br.readLine();
+            if (str.equals(".")) {
+                break;
+            }
+            bw.write(isBalancedString(str) ? "yes\n" : "no\n");
+        }
+
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    private static boolean isBalancedString(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '[') {
+                stack.push(ch);
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                stack.pop();
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
