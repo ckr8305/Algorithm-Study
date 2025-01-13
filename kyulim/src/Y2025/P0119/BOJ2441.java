ea9cb6f5270901f4d28cb1c25f47e8dbb6ad532c
ckr8305 <ckr8305@naver.com>
2025-01-13 21:57:54 +0900

    백준 2441번

--- kyulim/src/Y2025/P0119/BOJ2441.java
+++ kyulim/src/Y2025/P0119/BOJ2441.java
@@ -11,6 +11,17 @@ public class BOJ2441 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write(" ");
+            }
+            for (int k = N; k > i; k--) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
         br.close();
         bw.flush();
         bw.close();
