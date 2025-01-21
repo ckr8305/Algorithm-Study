c186dc5652f4949375676d4ffd3ef708c68a57f8
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-01-21 11:01:08 +0900

    백준 2563번

---
+++ taeheon/src/Y2025/P0126/BOJ2563.java
@@ -0,0 +1,38 @@
+package Y2025.P0126;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2563 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+
+        boolean[][] arr = new boolean[100][100];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
