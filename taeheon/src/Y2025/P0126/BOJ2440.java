7079a9516d89cfe2c8c91ed01ee46b73c98a68d0
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-01-21 10:40:19 +0900

    백준 2440번

---
+++ taeheon/src/Y2025/P0126/BOJ2440.java
@@ -0,0 +1,25 @@
+package Y2025.P0126;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2440 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            for (int i = 0; i < n; i++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+            n--;
+        }
+
+        System.out.println(sb);
+    }
+}
