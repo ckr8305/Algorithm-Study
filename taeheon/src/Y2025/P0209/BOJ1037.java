30aee984b1137844b5b10e7097ab93fdb8460eae
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-01-27 21:16:54 +0900

    백준 1037번

--- taeheon/src/Y2025/P0209/BOJ1037.java
+++ taeheon/src/Y2025/P0209/BOJ1037.java
@@ -1,4 +1,4 @@
-package Y2025;
+package Y2025.P0209;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class P0209 {
+public class BOJ1037 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
