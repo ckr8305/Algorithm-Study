98943b58e24d2a1e5ff490c46e0a0712c5a42222
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-01-27 21:16:35 +0900

    백준 1037번

---
+++ taeheon/src/Y2025/P0209/BOJ1037.java
@@ -0,0 +1,27 @@
+package Y2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class P0209 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> list = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        list.sort(null);
+        System.out.println(list.get(n - 1) * list.get(0));
+    }
+}
