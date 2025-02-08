328ef5d21396f628e3bbb87261b3e6137bfa2453
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-02-09 08:32:38 +0900

    백준 1181번

---
+++ taeheon/src/Y2025/P0216/BOJ1181.java
@@ -0,0 +1,38 @@
+package Y2025.P0216;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BOJ1181 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String word = br.readLine();
+            if (list.contains(word)) continue;
+            list.add(word);
+        }
+
+        list.sort((o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);
+            }
+            return o1.length() - o2.length();
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
