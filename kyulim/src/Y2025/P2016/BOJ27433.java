c81612aaed19379471c31512efe4f0843684ab6d
ckr8305 <ckr8305@naver.com>
2025-02-16 22:33:54 +0900

    백준 27433번

--- kyulim/src/Y2025/P2016/BOJ27433.java
+++ kyulim/src/Y2025/P2016/BOJ27433.java
@@ -11,9 +11,19 @@ public class BOJ27433 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
+        long result = factorial(N);
+
+        bw.write(result + "\n");
 
         br.close();
         bw.flush();
         bw.close();
     }
+
+    private static long factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
 }
+
