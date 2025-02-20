58f840d9f67f55596f58875b4964e74d5971dc37
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-02-20 20:18:43 +0900

    백준 15651번

---
+++ taeheon/src/Y2025/P0302/BOJ15651.java
@@ -0,0 +1,42 @@
+package Y2025.P0302;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ15651 {
+    static int n;
+    static int m;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n];
+        arr = new int[m];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+}
