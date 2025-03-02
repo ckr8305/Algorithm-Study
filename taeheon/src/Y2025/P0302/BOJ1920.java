8dc72e223a6f155d822efdbcb714f4f606db60b3
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-02 11:07:37 +0900

    백준 1920번

---
+++ taeheon/src/Y2025/P0302/BOJ1920.java
@@ -0,0 +1,37 @@
+package Y2025.P0302;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BOJ1920 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Set<Long> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            set.add(Long.parseLong(st.nextToken()));
+        }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if (set.contains(Long.parseLong(st.nextToken()))) {
+                sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
