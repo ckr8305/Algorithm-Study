baeae431f66e733bc10d7eccfe378a740e3090f8
KTH1007 <taeheon@gimtaeheon-ui-MacBookPro.local>
2025-03-06 17:37:20 +0900

    백준 9184번

---
+++ taeheon/src/Y2025/P0316/BOJ9184.java
@@ -0,0 +1,47 @@
+package Y2025.P0316;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ9184 {
+    static int[][][] dp = new int[21][21][21];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a;
+        int b;
+        int c;
+
+        dp[0][0][0] = 1;
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            if (a == -1 && b == -1 && c == -1) break;
+
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a, b, c)).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        } else if (a > 20 || b > 20 || c > 20) {
+            return dp[20][20][20] = w(20, 20, 20);
+        } else if (dp[a][b][c] != 0) return dp[a][b][c];
+        else if (a < b && b < c) {
+            return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+    }
+}
